2a01a7a0d067948c44ef8af9c489e0ac7b853820
ryland <rdbabusiak@bsu.edu>
2019-11-16 21:31:44 -0500

    Init

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    int multiply(int x, int y){
+        return x * y;
+    }
+
+    int add(int x, int y){
+        return x + y;
+    }
+
+    int divide(int x, int y){
+        return x / y;
+    }
+
+    int subtract(int x, int y){
+        return x - y;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,42 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    void addTest(){
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(19,11);
+
+        Assert.assertEquals(30, result);
+    }
+
+    @Test
+    void multiplyTest(){
+        Calculator calculator = new Calculator();
+
+        int result = calculator.multiply(3,4);
+
+        Assert.assertEquals(12, result);
+    }
+
+    @Test
+    void subtractTest(){
+        Calculator calculator = new Calculator();
+
+        int result = calculator.subtract(19,11);
+
+        Assert.assertEquals(8, result);
+    }
+
+    @Test
+    void divideTest(){
+        Calculator calculator = new Calculator();
+
+        int result = calculator.divide(121,11);
+
+        Assert.assertEquals(11, result);
+    }
+}
